f1ed3a874a8c347a2c680513e6dc208a36f347f2
akhilgouda28 <akhilgouda19@gmail.com>
2026-05-06 21:31:09 +0530

    Fix port and host for Railway deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import logging
 from flask import Flask
 from flask_login import LoginManager
@@ -47,4 +48,5 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
